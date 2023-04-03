7833d32231087a4820f0a903156916fb1f22b9ce
Hotávio Pacheco <8taviop@gmail.com>
2023-04-03 16:17:00 -0300

    middleware de cors

--- src/pages/api/pesquisa.ts
+++ src/pages/api/pesquisa.ts
@@ -3,6 +3,7 @@ import { conectarMongoDB } from "../../../middlewares/conectarMongoDB";
 import { validarTokenJWT } from "../../../middlewares/validarTokenJWT";
 import { UsuarioModel } from "../../../models/UsuarioModel";
 import type {RespostaPadraoMsg} from "../../../types/RespostaPadraoMsg";
+import { politicaCORS } from "../../../middlewares/politicaCORS";
 
 const pesquisaEndpoint
     = async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg | any[]>) => {
@@ -37,4 +38,4 @@ const pesquisaEndpoint
     }
 }
 
-export default validarTokenJWT(conectarMongoDB(pesquisaEndpoint));
+export default politicaCORS(validarTokenJWT(conectarMongoDB(pesquisaEndpoint)));
